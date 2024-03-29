6d3d27ba36a0a97a60dee952954503cb8ef974dd
Wojciech Łącki <49338133+Phabes@users.noreply.github.com>
2024-03-29 19:45:59 +0100

    Feat/panel improvements (#12)
    * levels data updated and creating new round of enemies setup
    
    * enemy with prototype move added
    
    * Damage and game over added
    
    * Next rounds start and game over added
    
    * Way of path recognition changed
    
    * player level up added
    
    * enemies spawn improved and player hp regeneration added
    
    * Panel UI changed
    
    * Removed unnecessary elements
    
    * Level selection improved
    
    * Loading spinner added
    
    * Messages added and UI improved
    
    * Enemies and player HP reduced

--- frontend/js/types/index.ts
+++ frontend/js/types/index.ts
@@ -2,5 +2,7 @@ export type { Coord } from "./coord";
 export type { Level } from "./level";
 export type { Map } from "./map";
 export type { Square } from "./square";
+export type { Status } from "./status";
 export type { Surface } from "./surface";
+export type { Variant } from "./variant";
 export type { Wave } from "./wave";
